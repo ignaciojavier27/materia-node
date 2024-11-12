5401d7b90eb8ef0ca83fd550b79f993163196c77
ignaciojavier27 <nacholin124@gmail.com>
2024-11-12 13:56:56 -0300

    Estructura general

--- 06_CorsAPI/app.js
+++ 06_CorsAPI/app.js
@@ -1,13 +1,30 @@
 import express from 'express';
+import movies from './movies.json' assert { type: 'json' };
+
 
 const app = express();
 app.disable('x-powered-by');
 
+const PORT = process.env.PORT ?? 1234;
+
 app.get('/', ( req, res ) => {
     res.json({ mensaje: 'Hola Mundo' })
-})
+});
 
-const PORT = process.env.PORT ?? 1234;
+app.get('/movies', ( req, res ) => {
+    res.json({ movies })
+});
+
+app.get('/movies/:id', ( req, res ) => {
+    const { id } = req.params;
+    const movie = movies.find(movie => movie.id === id);
+
+    if(movie) return res.json(movie)
+
+    res.status(404).json({ message : 'Película no encontrada.'})
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto http://localhost:${PORT}`)
